67c73e6405a380334cebd9382d4cc4777ef871b4
RondBallon <clement.ferrandery@gmail.com>
2024-10-07 14:36:16 +0200

    "ok"

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // Constantes de configuration de la base de données
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'socialnetwork');
-define('DB_USER', 'Babou');
-define('DB_PASS', '900613');
+define('DB_USER', 'admin');
+define('DB_PASS', 'admin');
 
 // Vous pouvez ajouter d'autres constantes de configuration ici si nécessaire
 ?>
