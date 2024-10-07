d6ae29c47b8c7c263439ea5e111bdc40d9d49be8
CeliaLachenal <59375489+CeliaLachenal@users.noreply.github.com>
2024-10-07 15:17:06 +0200

    dans wall.php ajout du coeur dans nombre de likes + modifications du fichier feed.php

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -102,7 +102,7 @@
                         <p><?php echo $post['content'] ?></p>
                         </div>
                         <footer>
-                            <small><?php echo $post['like_number'] ?></small>
+                            <small>♥<?php echo $post['like_number'] ?></small>
                             <a href=""><?php echo $post['taglist'] ?></a>,
                             
                         </footer>
